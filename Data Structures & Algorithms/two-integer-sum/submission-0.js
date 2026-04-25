41c343a270a32cf60bb02873edbec26f3be14d7c
Kailash Parihar <63588849+thekailashparihar@users.noreply.github.com>
2026-04-25 16:24:04 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = 0; j < nums.length; j++) {
+                if (i !== j && nums[i] + nums[j] === target) {
+                    return [i, j]
+                }
+            }
+        }
+    }
+}
